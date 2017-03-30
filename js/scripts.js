cdb4ae3a9bde0bfe08f0333711a715cdf5e81312
James Higgins and Ludlow Marko <ludlow@>
2017-03-30 11:48:30 -0700

    add js for 1 digit word conversions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 //Back End Logic
 
 
+var posOne = ["-","i","ii","iii","iv","v","vi","vii","viii","ix"];
+var posTwo = ["-","x","xx","xxx","xl","l","lx","lxx","lxxx","xc"];
+var posThree = ["-","c","cc","ccc","cd","d","dc","dcc","dccc","cm"];
+var Posfour = ["-","m","mm", "mmm"];
 
+var romanNum = function(input) {
+
+var splitNum = input.split("");
+if (splitNum.length === 1) {
+   var fp = splitNum[0];
+  var fpRoman = posOne[fp];
+  alert(fpRoman);
+  }
+};
 
 
 
 //Front End Logic
 $(document).ready(function(){
-
-  $("#userInput").submit(function(event) {
-    var input = $("#userInput input").val();
-    event.preventDefault();
-    alert(input);
+$("#form").submit(function(event) {
+  event.preventDefault();
+    var input = $("#userInput").val();
+    romanNum(input);
 });
 });
